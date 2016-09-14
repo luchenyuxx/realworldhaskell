e6bfefcceb6a1790d7cc2ad295e3005c7eb7ef37
chenyu <luchenyuxx@gmail.com>
2016-09-14 18:34:47 +0200

    progress on FindFile

--- src/FindFile/BetterPredicate.hs
+++ src/FindFile/BetterPredicate.hs
@@ -2,14 +2,41 @@ module FindFile.BetterPredicate where
 
 import Control.Monad (filterM)
 import System.Directory (Permissions(..), getModificationTime, getPermissions)
-import System.Time (ClockTime(..))
+import Data.Time.Clock (UTCTime(..))
 import System.FilePath (takeExtension)
 import FindFile.RecursiveContents (getRecursiveContents)
-import Control.Exception (bracket, handle)
+import Control.Exception (IOException, bracket, handle)
 import System.IO (IOMode(..), hClose, hFileSize, openFile)
 
 type Predicate = FilePath  -- path to directory entry
                -> Permissions -- permissions
                -> Maybe Integer -- file size (Nothing if not file)
-               -> ClockTime -- last modified
+               -> UTCTime -- last modified
                -> Bool
+
+type InfoP a = FilePath -> Permissions -> Maybe Integer -> UTCTime -> a
+
+pathP :: InfoP FilePath
+pathP path _ _ _ = path
+
+sizeP :: InfoP Integer
+sizeP _ _ (Just size) _ = size
+sizeP _ _ Nothing _ = -1
+
+equalP :: (Eq a) => InfoP a -> a -> InfoP Bool
+equalP f k w x y z = f w x y z == k
+
+getFileSize :: FilePath -> IO (Maybe Integer)
+getFileSize path = handle ((\_ -> return Nothing)::IOException -> IO (Maybe Integer)) $
+  bracket (openFile path ReadMode) hClose $ \h -> do
+    size <- hFileSize h
+    return (Just size)
+
+betterFind :: Predicate -> FilePath -> IO [FilePath]
+betterFind p path = getRecursiveContents path >>= filterM check
+  where check name = do
+          perms <- getPermissions name
+          size <- getFileSize name
+          modified <- getModificationTime name
+          return (p name perms size modified)
+
