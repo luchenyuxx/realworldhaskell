3ae7188a79d0c261261322cf75d8627581f8a7ca
chenyu <chenyu.lu@density-tech.com>
2016-10-24 22:38:15 +0200

    fix controlledvisit

--- src/FindFile/ControlledVisit.hs
+++ src/FindFile/ControlledVisit.hs
@@ -1,23 +1,26 @@
 module FindFile.ControlledVisit where
 
-import System.Directory (Permissions(..), getDirectoryContents, searchable)
+import Control.Monad
+import System.IO
+import System.Directory
 import Data.Time.Clock (UTCTime(..))
 import System.FilePath
+import Control.Exception
 
-Data Info = Info {
+data Info = Info {
   infoPath :: FilePath
   , infoPerms :: Maybe Permissions
   , infoSize :: Maybe Integer
   , infoModTime :: Maybe UTCTime
   } deriving (Eq, Ord, Show)
 
-traverse :: ([Info] -> [Info]) -> FilePath -> IO [Info]
-traverse order path = do
+traverseF :: ([Info] -> [Info]) -> FilePath -> IO [Info]
+traverseF order path = do
   names <- getUsefulContents path
   contents <- mapM getInfo (path : map (path </>) names)
   liftM concat $ forM (order contents) $ \info -> do
     if isDirectory info && infoPath info /= path
-      then traverse order (infoPath info)
+      then traverseF order (infoPath info)
       else return [info]
 
 getUsefulContents :: FilePath -> IO [String]
@@ -27,3 +30,13 @@ getUsefulContents path = do
 
 isDirectory :: Info -> Bool
 isDirectory = maybe False searchable . infoPerms
+
+maybeIO :: IO a -> IO (Maybe a)
+maybeIO act = handle ((\_ -> return Nothing)::IOException -> IO (Maybe a)) (Just `liftM` act)
+
+getInfo :: FilePath -> IO Info
+getInfo path = do
+  perms <- maybeIO (getPermissions path)
+  size <- maybeIO (bracket (openFile path ReadMode) hClose hFileSize)
+  modified <- maybeIO (getModificationTime path)
+  return (Info path perms size modified)
